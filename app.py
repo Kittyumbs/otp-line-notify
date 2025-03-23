395d73040555b6a0c69d9454bb4f86f736a97278
BeDuy <duylulu100901@gmail.com>
2025-03-23 14:41:27 +0700

    Update code to decode Base64 credentials from Config Vars

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import base64
+import json
 import requests
 import re
 import time
@@ -7,7 +9,6 @@ from google.oauth2 import service_account  # Dùng service_account
 from googleapiclient.discovery import build
 from tenacity import retry, stop_after_attempt, wait_fixed
 import logging
-import json
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
@@ -20,22 +21,32 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 app = Flask(__name__)
 
+def get_service_account_credentials():
+    """
+    Lấy chuỗi Base64 từ biến môi trường và giải mã nó thành thông tin đăng nhập của Service Account.
+    """
+    base64_string = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+
+    if base64_string:
+        # Giải mã chuỗi Base64 thành dữ liệu JSON
+        json_data = base64.b64decode(base64_string).decode('utf-8')
+        creds_info = json.loads(json_data)
+        
+        # Sử dụng service account credentials
+        creds = service_account.Credentials.from_service_account_info(creds_info, scopes=SCOPES)
+        return creds
+    else:
+        logging.error("Không tìm thấy biến môi trường GOOGLE_APPLICATION_CREDENTIALS.")
+        return None
+
 def gmail_authenticate(user_id):
-    creds_info_str = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
-    if not creds_info_str:
-        logging.error("GOOGLE_APPLICATION_CREDENTIALS not set.")
+    """
+    Xác thực ứng dụng với Gmail API bằng Service Account Credentials.
+    """
+    creds = get_service_account_credentials()
+    if not creds:
+        logging.error("Không thể lấy thông tin xác thực từ Service Account.")
         return None
-
-    try:
-        creds_info = json.loads(creds_info_str)
-        creds = service_account.Credentials.from_service_account_info(
-            creds_info, scopes=SCOPES
-        )
-    except Exception as e:
-        logging.error(f"Error creating credentials: {e}")
-        return None
-
-    # Không cần refresh token cho service account
 
     try:
         service = build('gmail', 'v1', credentials=creds)
@@ -73,8 +84,7 @@ def get_otp_emails(service, line_token):
 
                     if send_status == 200:
                         service.users().messages().modify(
-                            userId='me', id=message['id'], body={'removeLabelIds': ['UNREAD']}
-                        ).execute()
+                            userId='me', id=message['id'], body={'removeLabelIds': ['UNREAD']}).execute()
                         logging.info(f"Đã gửi OTP {otp_code} và đánh dấu email là đã đọc.")
                     else:
                         logging.warning(f"Gửi OTP {otp_code} thất bại. Giữ email chưa đọc.")
